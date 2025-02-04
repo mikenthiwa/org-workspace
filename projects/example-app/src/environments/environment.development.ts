17f603bf3cbe061a4ddf4d31b7df64a432746257
michael <mike.nthiwa@gmail.com>
2025-02-04 15:54:31 +0300

    Remove environment from git

--- projects/example-app/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://marketplace-v2.buupass.com/marketplace/',
-};
--- projects/example-app/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://marketplace-v2.buupass.com/marketplace/',
-};
